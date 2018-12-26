ba43d1d835441271544329fd97b5f3e53b84a28f
lali2 <lali2@cisco.com>
2018-12-26 14:22:51 +0800

    add pypi page link

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,12 @@
 
 #https://packaging.python.org/tutorials/packaging-projects/
-rm -rf ./dist/*
+
 #python setup.py register
 python setup.py sdist bdist_wheel upload
 
 #python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 python -m twine upload dist/*
+
+rm -rf ./dist
+rm -rf ./build
+rm -rf ./*.egg-info
